c2cca15e0ba3ec48988a4827a0631539c3d3fec9
Mayur Patel <mrpl272@gmail.com>
2024-06-14 16:48:18 -0400

    Add GitHub Actions workflow to create JIRA project

---
+++ get_jira_project.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+def main():
+    url = "https://mrpl272.atlassian.net/rest/api/3/project"
+
+    auth = HTTPBasicAuth(os.getenv("JIRA_USER"), os.getenv("JIRA_API_TOKEN"))
+
+    headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+
+    response = requests.get(
+       url,
+       headers=headers,
+       auth=auth
+    )
+
+    print(json.dumps(response.json(), sort_keys=True, indent=4, separators=(",", ": ")))
+
+if __name__ == "__main__":
+    main()

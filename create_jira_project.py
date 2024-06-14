dd2cb5aaf505146836992dc7abedb6e6dcdbde5d
Mayur Patel <mrpl272@gmail.com>
2024-06-14 15:51:49 -0400

    Add GitHub Actions workflow to create JIRA project

---
+++ create_jira_project.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://your-domain.atlassian.net/rest/api/3/project"
+
+auth = HTTPBasicAuth(os.getenv("JIRA_USER"), os.getenv("JIRA_API_TOKEN"))
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps({
+  "assigneeType": "PROJECT_LEAD",
+  "avatarId": 10200,
+  "categoryId": 10120,
+  "description": "Cloud migration initiative",
+  "issueSecurityScheme": 10001,
+  "key": "EX",
+  "leadAccountId": "5b10a0effa615349cb016cd8",
+  "name": "Example",
+  "notificationScheme": 10021,
+  "permissionScheme": 10011,
+  "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-simplified-process-control",
+  "projectTypeKey": "business",
+  "url": "http://atlassian.com"
+})
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))

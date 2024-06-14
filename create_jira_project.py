f9355ae843bb87e5438a918b0f56c1efb6fb66e0
Mayur Patel <mrpl272@gmail.com>
2024-06-14 16:40:24 -0400

    Add GitHub Actions workflow to create JIRA project

--- create_jira_project.py
+++ create_jira_project.py
@@ -3,37 +3,27 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://mrpl272.atlassian.net/rest/api/3/project"
+def main():
+    url = "https://mrpl272.atlassian.net/rest/api/3/project"
 
-auth = HTTPBasicAuth(os.getenv("JIRA_USER"), os.getenv("JIRA_API_TOKEN"))
+    auth = HTTPBasicAuth(os.getenv("JIRA_USER"), os.getenv("JIRA_API_TOKEN"))
 
-headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json"
-}
+    headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
 
-payload = json.dumps({
-  "assigneeType": "PROJECT_LEAD",
-  "avatarId": 10200,
-#   "categoryId": 10120,
-  "description": "Cloud migration initiative",
-#   "issueSecurityScheme": 10001,
-  "key": "EX",
-  "leadAccountId": "712020:d662bf67-24fd-4405-abf5-a3e80dd4be3c",
-  "name": "Example",
-#   "notificationScheme": 10021,
-#   "permissionScheme": 10011,
-  "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-simplified-process-control",
-  "projectTypeKey": "business",
-#   "url": "http://atlassian.com"
-})
+    with open('payload.json') as f:
+        payload = json.load(f)
 
-response = requests.request(
-   "POST",
-   url,
-   data=payload,
-   headers=headers,
-   auth=auth
-)
+    response = requests.post(
+       url,
+       json=payload,
+       headers=headers,
+       auth=auth
+    )
 
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+    print(json.dumps(response.json(), sort_keys=True, indent=4, separators=(",", ": ")))
+
+if __name__ == "__main__":
+    main()

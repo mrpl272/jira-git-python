a3f8b854844a768f733a20690ad5389dffa541b1
mrpl272 <68594163+mrpl272@users.noreply.github.com>
2024-07-26 15:33:50 -0400

    Update ado.py

--- ado.py
+++ ado.py
@@ -6,20 +6,20 @@ headers = {
     "Authorization": "Basic syu4bvgl6rzsawz4umkh2qy4ihzurxca6wiuh6l4pwsjfd2z5flq",
 }
 
-payload={
-  "name": "FabrikamTravel",
-  "description": "Frabrikam travel app for Windows Phone",
-  "capabilities": {
-    "versioncontrol": {
-      "sourceControlType": "Git"
-    },
-    "processTemplate": {
-      "templateTypeId": "6b724908-ef14-45cf-84f8-768b5384da45"
-    }
-  }
-}
+# payload={
+#   "name": "FabrikamTravel",
+#   "description": "Frabrikam travel app for Windows Phone",
+#   "capabilities": {
+#     "versioncontrol": {
+#       "sourceControlType": "Git"
+#     },
+#     "processTemplate": {
+#       "templateTypeId": "6b724908-ef14-45cf-84f8-768b5384da45"
+#     }
+#   }
+# }
 
-response = requests.get("https://dev.azure.com/mrpl2720314/_apis/projects?api-version=7.0", data=payload, headers=headers)
+response = requests.get("https://dev.azure.com/mrpl2720314/_apis/projects?api-version=7.0", headers=headers)
 
 print(response.status_code)
 print(response.text)
